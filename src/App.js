1a54bede5a92b3e6016cc8efe768c5ef364a9e2d
Yasmine <yasmineharroudi2020@gmail.com>
2026-01-18 11:17:46 +0100

    2nd commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Nav from './components/Nav';
 
 function App() {
   return (
+    
     <div className="App">
       {/* navbar */}
       <Nav/>
+    
 
       {/* banner */}
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import './Nav.scss'
+import React from 'react';
+import './Nav.scss';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
 function Nav() {
   return (
-    <div className='nav show nav--black '>
-        <button className='nav_burger'>---</button>
+    <div className='nav nav--black '>
+        <button className='nav_burger'>
+            <MenuIcon />
+        </button>
         <img src="./images/logo.png" alt="Netflix" className='nav_logo' />
         <nav className='nav_links'>
             <a href="" className="nav_link">Accueil</a>
@@ -11,10 +18,10 @@ function Nav() {
             <a href="" className="nav_link">Films</a>
         </nav>
         <div className="nav_action">
-            <a href="" className="nav_action">Search</a>
+            <a href="" className="nav_action"><SearchIcon/></a>
             <a href="" className="nav_action">DIRECT</a>
-            <a href="" className="nav_action">Gift</a>
-            <a href="" className="nav_action">Notif</a>
+            <a href="" className="nav_action"><CardGiftcardIcon/></a>
+            <a href="" className="nav_action"><NotificationsIcon/></a>
             <a href="" className="nav_action">
                 <img src="./images/avatar.jpg" alt="avatar" width='70' />
             </a>
