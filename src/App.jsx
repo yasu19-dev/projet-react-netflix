c3e0ca3469255badf4b6d2d81bd0b9abca483dd6
Yasmine <yasmineharroudi2020@gmail.com>
2026-01-20 19:54:39 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.scss';
 import Banner from './components/Banner';
